d785e48d8777111301a749d7e3d08a82e6a75a73
Anton udén <udenanton@hotmail.com>
2021-12-05 13:17:00 +0100

    Initial commit

---
+++ NovaCore-Spigot_v1_17_R1/src/main/java/net/zeeraa/novacore/spigot/version/v1_17_R1/VersionIndependentUtils.java
@@ -0,0 +1,488 @@
+package net.zeeraa.novacore.spigot.version.v1_17_R1;
+
+import java.util.UUID;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+
+import net.zeeraa.novacore.spigot.abstraction.VersionIndependantItems;
+import net.zeeraa.novacore.spigot.abstraction.enums.ColoredBlockType;
+import net.zeeraa.novacore.spigot.abstraction.enums.PlayerDamageReason;
+import net.zeeraa.novacore.spigot.abstraction.enums.VersionIndependantMetarial;
+import net.zeeraa.novacore.spigot.abstraction.enums.VersionIndependantSound;
+import net.zeeraa.novacore.spigot.abstraction.log.AbstractionLogger;
+import net.minecraft.server.MinecraftServer;
+import net.zeeraa.novacore.spigot.abstraction.ItemBuilderRecordList;
+
+public class VersionIndependentUtils extends net.zeeraa.novacore.spigot.abstraction.VersionIndependantUtils {
+	private ItemBuilderRecordList itemBuilderRecordList;
+	private boolean damagePlayerWarningShown = false;
+
+	public VersionIndependentUtils() {
+		itemBuilderRecordList = new ItemBuilderRecordListv1_17();
+	}
+
+	@Override
+	public ItemBuilderRecordList getItembBuilderRecordList() {
+		return itemBuilderRecordList;
+	}
+
+	@Override
+	public void setBlockAsPlayerSkull(Block block) {
+		block.setType(Material.PLAYER_HEAD);
+
+		block.getState().update(true);
+	}
+
+	@Override
+	public ItemStack getItemInMainHand(Player player) {
+		return player.getInventory().getItemInMainHand();
+	}
+
+	@Override
+	public ItemStack getItemInOffHand(Player player) {
+		return player.getInventory().getItemInOffHand();
+	}
+
+	@Override
+	public double getEntityMaxHealth(LivingEntity livingEntity) {
+		return livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+	}
+
+	@Override
+	public void setEntityMaxHealth(LivingEntity livingEntity, double health) {
+		livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(health);
+	}
+
+	@Override
+	public void resetEntityMaxHealth(LivingEntity livingEntity) {
+		livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
+	}
+
+	@SuppressWarnings({ "deprecation", "resource" })
+	@Override
+	public double[] getRecentTps() {
+		// Deprecated but still the way spigot does it in the /tps command
+		return MinecraftServer.getServer().recentTps;
+	}
+
+	@Override
+	public int getPlayerPing(Player player) {
+		return player.getPing();
+	}
+
+	@Override
+	public void cloneBlockData(Block source, Block target) {
+		target.setBlockData(source.getBlockData());
+	}
+
+	@Override
+	public void setItemInMainHand(Player player, ItemStack item) {
+		player.getInventory().setItemInMainHand(item);
+	}
+
+	@Override
+	public void setItemInOffHand(Player player, ItemStack item) {
+		player.getInventory().setItemInOffHand(item);
+	}
+
+	@Override
+	public void sendTabList(Player player, String header, String footer) {
+		player.setPlayerListHeaderFooter(header, footer);
+	}
+
+	@Override
+	public void damagePlayer(Player player, PlayerDamageReason reason, float damage) {
+		// TODO: implement this
+
+		if (!damagePlayerWarningShown) {
+			damagePlayerWarningShown = true;
+			AbstractionLogger.getLogger().warning("Nova VersionIndependentUtils v1_17_R1", "damagePlayer will not work on 1.17 and will instead call Player#damage(double)");
+		}
+
+		player.damage(damage);
+
+		/*
+		 * DamageSource source;
+		 * 
+		 * switch (reason) { case FALL: source = DamageSource.FALL;
+		 * 
+		 * case FALLING_BLOCK: source = DamageSource.FALLING_BLOCK; break; case
+		 * OUT_OF_WORLD: source = DamageSource.OUT_OF_WORLD; break;
+		 * 
+		 * case BURN: source = DamageSource.BURN; break;
+		 * 
+		 * case LIGHTNING: source = DamageSource.LIGHTNING; break;
+		 * 
+		 * case MAGIC: source = DamageSource.MAGIC; break;
+		 * 
+		 * case DROWN: source = DamageSource.DROWN; break;
+		 * 
+		 * case STARVE: source = DamageSource.STARVE; break;
+		 * 
+		 * case LAVA: source = DamageSource.LAVA; break;
+		 * 
+		 * case GENERIC: source = DamageSource.GENERIC; break;
+		 * 
+		 * default: source = DamageSource.GENERIC; break; }
+		 * 
+		 * ((CraftPlayer) player).getHandle().damageEntity(source, damage);
+		 */
+	}
+
+	@Override
+	public void setColoredBlock(Block block, DyeColor color, ColoredBlockType type) {
+		Material material = getColoredMaterial(color, type);
+
+		block.setType(material);
+	}
+
+	@Override
+	public ItemStack getColoredItem(DyeColor color, ColoredBlockType type) {
+		return new ItemStack(getColoredItem(color, type));
+	}
+
+	@Override
+	public void setShapedRecipeIngredientAsColoredBlock(ShapedRecipe recipe, char ingredient, ColoredBlockType type, DyeColor color) {
+		recipe.setIngredient(ingredient, getColoredMaterial(color, type));
+	}
+
+	@Override
+	public void addShapelessRecipeIngredientAsColoredBlock(ShapelessRecipe recipe, char ingredient, ColoredBlockType type, DyeColor color) {
+		recipe.addIngredient(getColoredMaterial(color, type));
+	}
+
+	private Material getColoredMaterial(DyeColor color, ColoredBlockType type) {
+		Material material;
+
+		if (type == ColoredBlockType.GLASS_BLOCK) {
+			switch (color) {
+			case BLACK:
+				material = Material.BLACK_STAINED_GLASS;
+				break;
+
+			case BLUE:
+				material = Material.BLUE_STAINED_GLASS;
+				break;
+
+			case BROWN:
+				material = Material.BROWN_STAINED_GLASS;
+				break;
+
+			case CYAN:
+				material = Material.CYAN_STAINED_GLASS;
+				break;
+
+			case GRAY:
+				material = Material.LIGHT_GRAY_STAINED_GLASS;
+				break;
+
+			case GREEN:
+				material = Material.GREEN_STAINED_GLASS;
+				break;
+
+			case LIGHT_BLUE:
+				material = Material.LIGHT_BLUE_STAINED_GLASS;
+				break;
+
+			case LIGHT_GRAY:
+				material = Material.LIGHT_GRAY_STAINED_GLASS;
+				break;
+
+			case LIME:
+				material = Material.LIME_STAINED_GLASS;
+				break;
+
+			case MAGENTA:
+				material = Material.MAGENTA_STAINED_GLASS;
+				break;
+
+			case ORANGE:
+				material = Material.ORANGE_STAINED_GLASS;
+				break;
+
+			case PINK:
+				material = Material.PINK_STAINED_GLASS;
+				break;
+
+			case PURPLE:
+				material = Material.PURPLE_STAINED_GLASS;
+				break;
+
+			case RED:
+				material = Material.RED_STAINED_GLASS;
+				break;
+
+			case YELLOW:
+				material = Material.YELLOW_STAINED_GLASS;
+				break;
+
+			default:
+				material = Material.AIR;
+				break;
+			}
+		} else if (type == ColoredBlockType.GLASS_PANE) {
+			switch (color) {
+			case BLACK:
+				material = Material.BLACK_STAINED_GLASS_PANE;
+				break;
+
+			case BLUE:
+				material = Material.BLUE_STAINED_GLASS_PANE;
+				break;
+
+			case BROWN:
+				material = Material.BROWN_STAINED_GLASS_PANE;
+				break;
+
+			case CYAN:
+				material = Material.CYAN_STAINED_GLASS_PANE;
+				break;
+
+			case GRAY:
+				material = Material.LIGHT_GRAY_STAINED_GLASS_PANE;
+				break;
+
+			case GREEN:
+				material = Material.GREEN_STAINED_GLASS_PANE;
+				break;
+
+			case LIGHT_BLUE:
+				material = Material.LIGHT_BLUE_STAINED_GLASS_PANE;
+				break;
+
+			case LIGHT_GRAY:
+				material = Material.LIGHT_GRAY_STAINED_GLASS_PANE;
+				break;
+
+			case LIME:
+				material = Material.LIME_STAINED_GLASS_PANE;
+				break;
+
+			case MAGENTA:
+				material = Material.MAGENTA_STAINED_GLASS_PANE;
+				break;
+
+			case ORANGE:
+				material = Material.ORANGE_STAINED_GLASS_PANE;
+				break;
+
+			case PINK:
+				material = Material.PINK_STAINED_GLASS_PANE;
+				break;
+
+			case PURPLE:
+				material = Material.PURPLE_STAINED_GLASS_PANE;
+				break;
+
+			case RED:
+				material = Material.RED_STAINED_GLASS_PANE;
+				break;
+
+			case YELLOW:
+				material = Material.YELLOW_STAINED_GLASS_PANE;
+				break;
+
+			default:
+				material = Material.AIR;
+				break;
+			}
+		} else if (type == ColoredBlockType.WOOL) {
+			switch (color) {
+			case BLACK:
+				material = Material.BLACK_WOOL;
+				break;
+
+			case BLUE:
+				material = Material.BLUE_WOOL;
+				break;
+
+			case BROWN:
+				material = Material.BROWN_WOOL;
+				break;
+
+			case CYAN:
+				material = Material.CYAN_WOOL;
+				break;
+
+			case GRAY:
+				material = Material.LIGHT_GRAY_WOOL;
+				break;
+
+			case GREEN:
+				material = Material.GREEN_WOOL;
+				break;
+
+			case LIGHT_BLUE:
+				material = Material.LIGHT_BLUE_WOOL;
+				break;
+
+			case LIGHT_GRAY:
+				material = Material.LIGHT_GRAY_WOOL;
+				break;
+
+			case LIME:
+				material = Material.LIME_WOOL;
+				break;
+
+			case MAGENTA:
+				material = Material.MAGENTA_WOOL;
+				break;
+
+			case ORANGE:
+				material = Material.ORANGE_WOOL;
+				break;
+
+			case PINK:
+				material = Material.PINK_WOOL;
+				break;
+
+			case PURPLE:
+				material = Material.PURPLE_WOOL;
+				break;
+
+			case RED:
+				material = Material.RED_WOOL;
+				break;
+
+			case YELLOW:
+				material = Material.YELLOW_WOOL;
+				break;
+
+			default:
+				material = Material.AIR;
+				break;
+			}
+		} else {
+			material = Material.AIR;
+		}
+
+		return material;
+	}
+
+	@Override
+	public void attachMapView(ItemStack item, MapView mapView) {
+		MapMeta meta = (MapMeta) item.getItemMeta();
+
+		meta.setMapView(mapView);
+
+		item.setItemMeta(meta);
+	}
+
+	@Override
+	public MapView getAttachedMapView(ItemStack item) {
+		MapMeta meta = (MapMeta) item.getItemMeta();
+
+		return meta.getMapView();
+	}
+
+	@Override
+	public int getMapViewId(MapView mapView) {
+		return mapView.getId();
+	}
+
+	@Override
+	public void playSound(Player player, Location location, VersionIndependantSound sound, float volume, float pitch) {
+		Sound realSound = null;
+
+		switch (sound) {
+		case NOTE_PLING:
+			realSound = Sound.BLOCK_NOTE_BLOCK_PLING;
+			break;
+
+		case NOTE_HAT:
+			realSound = Sound.BLOCK_NOTE_BLOCK_HAT;
+			break;
+
+		case WITHER_DEATH:
+			realSound = Sound.ENTITY_WITHER_DEATH;
+			break;
+
+		case WITHER_HURT:
+			realSound = Sound.ENTITY_WITHER_HURT;
+			break;
+
+		case ITEM_BREAK:
+			realSound = Sound.ENTITY_ITEM_BREAK;
+			break;
+
+		case ORB_PICKUP:
+			realSound = Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
+			break;
+
+		default:
+			System.err.println("[VersionIndependentUtils] VersionIndependantSound " + sound.name() + " is not defined in this version. Please add it to " + this.getClass().getName());
+			return;
+		}
+
+		player.playSound(location, realSound, volume, pitch);
+	}
+
+	@Override
+	public void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+		player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
+	}
+
+	@Override
+	public ItemStack getPlayerSkullWithBase64Texture(String b64stringtexture) {
+		GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+		PropertyMap propertyMap = profile.getProperties();
+		if (propertyMap == null) {
+			throw new IllegalStateException("Profile doesn't contain a property map");
+		}
+		propertyMap.put("textures", new Property("textures", b64stringtexture));
+		ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
+		ItemMeta headMeta = head.getItemMeta();
+		Class<?> headMetaClass = headMeta.getClass();
+		try {
+			getField(headMetaClass, "profile", GameProfile.class, 0).set(headMeta, profile);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		head.setItemMeta(headMeta);
+		return head;
+	}
+
+	@Override
+	public VersionIndependantItems getVersionIndependantItems() {
+		return new net.zeeraa.novacore.spigot.version.v1_17_R1.VersionIndependantItems();
+	}
+
+	@Override
+	public void setShapedRecipeIngredientAsPlayerSkull(ShapedRecipe recipe, char ingredient) {
+		recipe.setIngredient(ingredient, Material.PLAYER_HEAD);
+	}
+
+	@Override
+	public ItemStack getItemStack(VersionIndependantMetarial material) {
+		switch (material) {
+		case FILLED_MAP:
+			ItemStack stack = new ItemStack(Material.FILLED_MAP);
+			return stack;
+
+		case END_STONE:
+			return new ItemStack(Material.END_STONE);
+
+		default:
+			return null;
+		}
+	}
+}
