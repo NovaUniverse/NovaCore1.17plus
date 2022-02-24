0632907cbc81b86fc6f1e302e8466b4d80249cc6
Anton udén <udenanton@hotmail.com>
2022-02-24 12:15:57 +0100

    Moved 1.17+ into separate jar file

---
+++ NovaCore-1_17-Plus/src/net/novauniverse/novacore1_17plus/NovaCore1_17Plus.java
@@ -0,0 +1,10 @@
+package net.novauniverse.novacore1_17plus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NovaCore1_17Plus extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Added support for novacore 1.17+");
+	}
+}
