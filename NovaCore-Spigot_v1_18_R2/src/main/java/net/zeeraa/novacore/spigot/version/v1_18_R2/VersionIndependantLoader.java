dd3d51264f1363b4ff1e3284555822ef38c79e9a
Anton udén <udenanton@hotmail.com>
2022-05-03 18:03:27 +0200

    Add 1.18.2

---
+++ NovaCore-Spigot_v1_18_R2/src/main/java/net/zeeraa/novacore/spigot/version/v1_18_R2/VersionIndependantLoader.java
@@ -0,0 +1,21 @@
+package net.zeeraa.novacore.spigot.version.v1_18_R2;
+
+import net.zeeraa.novacore.spigot.abstraction.CommandRegistrator;
+import net.zeeraa.novacore.spigot.abstraction.Listeners;
+
+public class VersionIndependantLoader implements net.zeeraa.novacore.spigot.abstraction.VersionIndependantLoader {
+	@Override
+	public CommandRegistrator getCommandRegistrator() {
+		return new net.zeeraa.novacore.spigot.version.v1_18_R2.CommandRegistrator();
+	}
+
+	@Override
+	public VersionIndependentUtils getVersionIndependentUtils() {
+		return new net.zeeraa.novacore.spigot.version.v1_18_R2.VersionIndependentUtils();
+	}
+
+	@Override
+	public Listeners getListeners() {
+		return new net.zeeraa.novacore.spigot.version.v1_18_R2.Listeners();
+	}
+}
