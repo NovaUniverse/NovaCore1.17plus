fdd103329a25bb2c14001b44913d4dce07058fbb
BrunoGamerGH <bruno.manarino@gmail.com>
2023-04-21 18:48:27 -0300

    update

--- NovaCore-Spigot_v1_19_R3/src/main/java/net/zeeraa/novacore/spigot/version/v1_19_R3/packet/MinecraftChannelDuplexHandler.java
+++ NovaCore-Spigot_v1_19_R3/src/main/java/net/zeeraa/novacore/spigot/version/v1_19_R3/packet/MinecraftChannelDuplexHandler.java
@@ -1,9 +1,11 @@
 package net.zeeraa.novacore.spigot.version.v1_19_R3.packet;
 
+import net.minecraft.core.BlockPosition;
 import net.minecraft.network.protocol.game.PacketPlayInArmAnimation;
 import net.minecraft.network.protocol.game.PacketPlayInBlockDig;
 import net.minecraft.network.protocol.game.PacketPlayInSettings;
 import net.minecraft.network.protocol.game.PacketPlayInSpectate;
+import net.minecraft.network.protocol.game.PacketPlayInSteerVehicle;
 import net.minecraft.network.protocol.game.PacketPlayOutNamedSoundEffect;
 import net.minecraft.resources.MinecraftKey;
 import net.minecraft.sounds.SoundEffect;
@@ -12,10 +14,12 @@ import net.zeeraa.novacore.spigot.abstraction.enums.ChatVisibility;
 import net.zeeraa.novacore.spigot.abstraction.enums.Hand;
 import net.zeeraa.novacore.spigot.abstraction.enums.MainHand;
 import net.zeeraa.novacore.spigot.abstraction.enums.SoundCategory;
+import net.zeeraa.novacore.spigot.abstraction.packet.PacketManager;
 import net.zeeraa.novacore.spigot.abstraction.packet.event.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
@@ -32,55 +36,58 @@ public class MinecraftChannelDuplexHandler extends net.zeeraa.novacore.spigot.ab
 		super(player);
 	}
 
-	public boolean readPacket(Player player, Object packet) throws NoSuchFieldException, IllegalAccessException {
-		List<Event> events = new ArrayList<>();
-		if (packet.getClass().equals(PacketPlayInSettings.class)) {
-			PacketPlayInSettings settings = (PacketPlayInSettings) packet;
-
-			events.add(new PlayerSettingsEvent(player, settings.b, settings.c, ChatVisibility.fromNMS(settings.d().name()), settings.e(), settings.f(), MainHand.valueOfNMS(settings.g().name()), settings.h(), settings.i()));
-		} else if (packet.getClass().equals(PacketPlayInArmAnimation.class)) {
-			PacketPlayInArmAnimation arm = (PacketPlayInArmAnimation) packet;
-
-			events.add(new PlayerSwingEvent(player, System.currentTimeMillis(), Hand.valueOf(arm.a().name())));
-
-			if (canBreak(player, VersionIndependentUtils.get().getReacheableBlockExact(player))) {
-				events.add(new PlayerAttemptBreakBlockEvent(player, System.currentTimeMillis(), VersionIndependentUtils.get().getReacheableBlockExact(player)));
-			}
-		} else if (packet.getClass().equals(PacketPlayInSpectate.class)) {
-			PacketPlayInSpectate spectate = (PacketPlayInSpectate) packet;
-			Field field = PacketPlayInSpectate.class.getDeclaredField("a");
-			field.setAccessible(true);
-			UUID id = (UUID) field.get(spectate);
-			events.add(new SpectatorTeleportEvent(player, Bukkit.getPlayer(id)));
-
-		} else if (packet.getClass().equals(PacketPlayInBlockDig.class)) {
-			List<Player> playersDigging = VersionIndependentUtils.get().getPacketManager().getPlayersDigging();
-			PacketPlayInBlockDig action = (PacketPlayInBlockDig) packet;
-
-			switch (action.d()) {
-			case a:
-				if (playersDigging.stream().noneMatch(pl -> pl.getUniqueId().equals(player.getUniqueId()))) {
-					Block block = VersionIndependentUtils.get().getReacheableBlockExact(player);
-					if (block != null) {
-						if (canBreak(player, block)) {
-							events.add(new PlayerAttemptBreakBlockEvent(player, System.currentTimeMillis(), block));
-						}
-					}
+	public boolean readPacket(Player player, Object packet) {
+		List<PacketEvent> events = new ArrayList<>();
+		if (packet.getClass().equals(PacketPlayInSteerVehicle.class)) {
+			PacketPlayInSteerVehicle steer = (PacketPlayInSteerVehicle) packet;
+			float sideways = steer.a();
+			float forwards = steer.c();
+			boolean jump = steer.d();
+			boolean leave = steer.e();
+			if (sideways != 0 || forwards != 0 || jump || leave) {
+				PlayerInputEvent.Press side;
+				PlayerInputEvent.Press front;
+				if (sideways > 0) {
+					side = PlayerInputEvent.Press.LEFT;
+				} else if (sideways < 0) {
+					side = PlayerInputEvent.Press.RIGHT;
+				} else {
+					side = PlayerInputEvent.Press.NONE;
 				}
-				break;
-			case b:
-			case c:
-				playersDigging.remove(player);
-				break;
-
-			default:
-				break;
+				if (forwards > 0) {
+					front = PlayerInputEvent.Press.FRONT;
+				} else if (forwards < 0) {
+					front = PlayerInputEvent.Press.BACK;
+				} else {
+					front = PlayerInputEvent.Press.NONE;
+				}
+				events.add(new PlayerInputEvent(player, side, front, jump, leave));
+			}
+		} else if (packet.getClass().equals(PacketPlayInBlockDig.class)) {
+			PacketPlayInBlockDig dig = (PacketPlayInBlockDig) packet;
+			BlockPosition bp = dig.a();
+			Block block = player.getWorld().getBlockAt(bp.u(), bp.v(), bp.w());
+			BlockFace face = switch (dig.c()) {
+				case b -> BlockFace.UP;
+				case a -> BlockFace.DOWN;
+				case f -> BlockFace.EAST;
+				case e -> BlockFace.WEST;
+				case c -> BlockFace.NORTH;
+				case d -> BlockFace.SOUTH;
+			};
+			switch (dig.d()) {
+				case c -> events.add(new PlayerStopBlockDigEvent(player, block, face));
+				case b -> events.add(new PlayerAbortBlockDigEvent(player, block, face));
+				case a -> events.add(new PlayerStartBlockDigEvent(player, block, face));
 			}
 		}
-		for (Event e : events) {
-			Bukkit.getPluginManager().callEvent(e);
-			if (((Cancellable) e).isCancelled()) {
-				return false;
+
+		for (PacketEvent e : events) {
+			net.zeeraa.novacore.spigot.abstraction.packet.PacketManager.fireEvent(e);
+			if (e instanceof Cancellable) {
+				if (((Cancellable) e).isCancelled()) {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -88,29 +95,14 @@ public class MinecraftChannelDuplexHandler extends net.zeeraa.novacore.spigot.ab
 
 	@Override
 	public boolean writePacket(Player player, Object packet) {
-		List<Event> events = new ArrayList<>();
-		if (packet.getClass().equals(PacketPlayOutNamedSoundEffect.class)) {
-			PacketPlayOutNamedSoundEffect effect = (PacketPlayOutNamedSoundEffect) packet;
+		List<PacketEvent> events = new ArrayList<>();
 
-			SoundCategory soundCategory = Arrays.stream(SoundCategory.values()).filter(sc -> sc.getName().equalsIgnoreCase(effect.c().a())).findFirst().get();
-			SoundEffect effect1 = effect.a().a();
-			MinecraftKey mcKey = effect1.a();
-			Sound foundSound = Arrays.stream(Sound.values()).filter(sound -> sound.getKey().toString().equalsIgnoreCase(mcKey.toString())).findFirst().get();
-			net.zeeraa.novacore.spigot.abstraction.enums.SoundCategory category = Arrays.stream(SoundCategory.values()).filter(sc -> sc.getName().equalsIgnoreCase(soundCategory.getName())).findFirst().get();
-
-			double x = effect.d();
-			double y = effect.e();
-			double z = effect.f();
-			float volume = effect.g();
-			float pitch = effect.h();
-
-			events.add(new PlayerListenSoundEvent(player, foundSound, category, x, y, z, volume, pitch));
-		}
-
-		for (Event e : events) {
-			Bukkit.getPluginManager().callEvent(e);
-			if (((Cancellable) e).isCancelled()) {
-				return false;
+		for (PacketEvent e : events) {
+			PacketManager.fireEvent(e);
+			if (e instanceof Cancellable) {
+				if (((Cancellable) e).isCancelled()) {
+					return false;
+				}
 			}
 		}
 		return true;
